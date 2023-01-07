96efa6450e97bf88c814f37d7baea444ed913d88
Neeraj Kashyap <nkashy1@gmail.com>
2023-01-07 15:04:46 -0800

    Got started with Inventory implementation

---
+++ contracts/inventory/Inventory.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: UNLICENSED
+
+/**
+ * Authors: Moonstream DAO (engineering@moonstream.to)
+ * GitHub: https://github.com/G7DAO/contracts
+ */
+
+pragma solidity ^0.8.0;
+
+import {TerminusPermissions} from "@moonstream/contracts/terminus/TerminusPermissions.sol";
+import {DiamondReentrancyGuard} from "@moonstream-engine/contracts/diamond/security/DiamondReentrancyGuard.sol";
+import "@openzeppelin-contracts/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import "@openzeppelin-contracts/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin-contracts/contracts/token/ERC1155/IERC1155.sol";
+import "../diamond/libraries/LibDiamond.sol";
+
+library LibInventory {
+    bytes32 constant STORAGE_POSITION =
+        keccak256("g7dao.eth.storage.Inventory");
+
+    struct InventoryStorage {
+        address AdminTerminusAddress;
+        uint256 AdminTerminusPoolId;
+    }
+
+    function inventoryStorage()
+        internal
+        pure
+        returns (InventoryStorage storage istore)
+    {
+        bytes32 position = STORAGE_POSITION;
+        assembly {
+            istore.slot := position
+        }
+    }
+}
+
+contract InventoryFacet is
+    ERC721Holder,
+    ERC1155Holder,
+    TerminusPermissions,
+    DiamondReentrancyGuard
+{
+    modifier onlyAdmin() {
+        LibInventory.InventoryStorage storage istore = LibInventory
+            .inventoryStorage();
+        require(
+            _holdsPoolToken(
+                istore.AdminTerminusAddress,
+                istore.AdminTerminusPoolId,
+                1
+            ),
+            "InventoryFacet.onlyAdmin: The address is not an authorized administrator"
+        );
+        _;
+    }
+
+    event AdministratorDesignated(
+        address indexed adminTokenAddress,
+        uint256 indexed adminTokenPoolId
+    );
+
+    function init(address adminTerminusAddress, uint256 adminTerminusPoolId)
+        external
+    {
+        LibDiamond.enforceIsContractOwner();
+        LibInventory.InventoryStorage storage istore = LibInventory
+            .inventoryStorage();
+        istore.AdminTerminusAddress = adminTerminusAddress;
+        istore.AdminTerminusPoolId = adminTerminusPoolId;
+    }
+}
