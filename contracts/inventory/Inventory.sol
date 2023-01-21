661ad881681cc8d224b03c915405b4e2fd922164
Neeraj Kashyap <nkashy1@gmail.com>
2023-01-21 15:14:14 -0800

    Fixed https://github.com/G7DAO/contracts/issues/28

--- contracts/inventory/Inventory.sol
+++ contracts/inventory/Inventory.sol
@@ -332,7 +332,7 @@ contract InventoryFacet is
                 address(this),
                 msg.sender,
                 existingItem.ItemTokenId,
-                existingItem.Amount,
+                amount,
                 ""
             );
         }
